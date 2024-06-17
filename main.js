7969d686926aaf3a08eac5fade11c0caa03b2dcf
Elmar <elmar.goetz@gmail.com>
2024-06-17 16:47:21 +0200

    Database Sätze Part One & Part 2 erstellt

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('This is working!')
+let sentencePartOne = ["Indem wir weniger Plastik verwenden", "Durch den Verzicht auf Einwegprodukte", "Wenn wir auf erneuerbare Energien umsteigen", 
+    "Mit dem Pflanzen von Bäumen", "Durch nachhaltige Landwirtschaft", "Indem wir Recycling fördern", "Durch den Schutz von Wäldern",
+    "Wenn wir auf öffentliche Verkehrsmittel umsteigen", "Mit der Reduktion von Fleischkonsum", "Durch die Nutzung von Solarenergie" ]
+
+let sentencePartTwo = ["schützen wir unsere Ozeane.", "reduzieren wir die Müllmenge.", "verringern wir den CO2-Ausstoß.", 
+    "verbessern wir die Luftqualität.", "schonen wir die natürlichen Ressourcen.", "reduzieren wir den Abfall auf Deponien.", "bewahren wir die Artenvielfalt.",
+    "reduzieren wir den Verkehr.", "senken wir den Wasserverbrauch.", "sparen wir fossile Brennstoffe ein." ]
